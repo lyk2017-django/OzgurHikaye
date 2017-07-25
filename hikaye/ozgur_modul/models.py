111f592a7c548702deca2f86997a51b4dd34d00e
sonakinci41 <sonakinci41@gmail.com>
2017-07-25 17:21:23 +0300

    Tablolar tanımlandı Storys |_storyTitle |_createTime
    Contributions
    |_storyId
    |_contributionText
    |_createTime
    
    Rules.txt oluşturuldu

--- hikaye/ozgur_modul/models.py
+++ hikaye/ozgur_modul/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Storys(models.Model):
+    storyTitle_ = models.CharField(max_length=200)
+    createTime_ = models.DateField(auto_now_add=True)
+
+
+class Contributions(models.Model):
+    storyId_ = models.ManyToManyField("Storys")
+    contributionText_ = models.TextField()
+    createTime_ = models.DateField(auto_now_add=True)
