1d2e65dfa98e8ca76bca75125fab01eba77b781e
Nuri Akman <nuriakman@gmail.com>
2017-07-26 10:11:50 +0300

    Model güncellendi

--- hikaye/ozgur_modul/models.py
+++ hikaye/ozgur_modul/models.py
@@ -5,8 +5,15 @@ class Storys(models.Model):
     story_title = models.CharField(max_length=200)
     create_time = models.DateField(auto_now_add=True)
 
+    def __str__(self):
+        return "#{id} {text} ".format(id=self.id, text=self.story_title)
+
 
 class Contributions(models.Model):
-    story_id = models.ForeignKey("Storys")
+    story = models.ForeignKey("Storys")
     contribution_text = models.TextField()
     create_time = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return "#{id} {text} ".format(id=self.id, text=self.contribution_text)
+
