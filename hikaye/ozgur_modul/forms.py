fcc3030e1a4bfc941f80d1ed15d7cd7e28963d8b
Nuri Akman <nuriakman@gmail.com>
2017-07-29 17:17:31 +0300

    Tema eklendi ve tüm sayfalara uyarlandı

--- hikaye/ozgur_modul/forms.py
+++ hikaye/ozgur_modul/forms.py
@@ -6,6 +6,9 @@ class StoryNewForm(forms.ModelForm):
     class Meta:
         model = Storys
         exclude = ["id"]
+        labels = {
+            "story_title": "Hikayenizin Başlığı"
+        }
 
 class ContribituonsNewForm(forms.ModelForm):
     class Meta:
@@ -13,6 +16,9 @@ class ContribituonsNewForm(forms.ModelForm):
         exclude = ["id"]
         widgets = {
             "story": HiddenInput()
+        }
+        labels = {
+            "contribution_text": ""
         }
 
 
